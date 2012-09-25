19f1074a75a394a6902646c91e940c06de37b8db
Vikas K <vcoski@gmail.com>
2012-09-26 04:32:06 +0530

    Added GPL license information.

--- src/in/goahead/apps/yaytd/util/URLEncodeDecode.java
+++ src/in/goahead/apps/yaytd/util/URLEncodeDecode.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of Yet Another Youtube Downloader (yaytd).
+
+    Youtube Downloader (yaytd) 
+    is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Youtube Downloader (yaytd)
+     is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Youtube Downloader (yaytd).
+    If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package in.goahead.apps.yaytd.util;
 
 import java.util.HashMap;
